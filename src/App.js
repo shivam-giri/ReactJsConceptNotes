3c8f07c12c1654fd71cf8e822797044901ac47fd
shivam giri <mahant1598sivam@gmail.com>
2020-10-14 13:35:58 -0400

    UserCoponent added props concept

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 //root component
 import React from 'react';
-import ChildComponent from './component/ChildComponent';
 
+import UserComponent from './component/UserComponent';
 const App = () =>{
     return(
-        <div>
-            <h1>I am Root component</h1>
-           
-        </div>
-    );
-};
+        <section id = "ParentBlock">
+            <article>
+                <UserComponent
+                    name = "mani"
+                    age = {20}
+                    company = "cgi"
+                    salary = {100000}
+                    >
+                    <button>Hire Me</button>
+                </UserComponent>
+                
+                <UserComponent
+                    name = "shivam"
+                    age = {25}
+                    company = "google"
+                    salary = {100022200}
+                    >
+                    <button>Hire Me</button>
+                </UserComponent>
+
+                
+                <UserComponent
+                    name = "subham"
+                    age = {27}
+                    company = "RBI"
+                    salary = {10005500}
+                    >
+                    <button>Hire Me</button>
+                </UserComponent>
+            </article>
+
+        </section>
+    )}
 export default App;
